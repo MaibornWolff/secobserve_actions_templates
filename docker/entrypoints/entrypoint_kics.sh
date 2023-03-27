6233fa105e9d6070d9793259084609c367993a31
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2023-03-27 16:57:15 +0200

    fix: set output path to WORKSPACE

--- docker/entrypoints/entrypoint_kics.sh
+++ docker/entrypoints/entrypoint_kics.sh
@@ -15,7 +15,7 @@ export SO_FILE_NAME="${REPORT_NAME}"
 export SO_PARSER_NAME="Sarif"
 
 cd "$RUN_DIRECTORY"
-kics scan $FURTHER_PARAMETERS --no-progress --ignore-on-exit results --queries-path /usr/local/kics/queries --path "$TARGET" --report-formats sarif --output-path . --output-name "$WORKSPACE/$REPORT_NAME"
+kics scan $FURTHER_PARAMETERS --no-progress --ignore-on-exit results --queries-path /usr/local/kics/queries --path "$TARGET" --report-formats sarif --output-path "$WORKSPACE" --output-name "$REPORT_NAME"
 cd "$WORKSPACE"
 
 if [ "$SO_UPLOAD" == "true" ]; then
