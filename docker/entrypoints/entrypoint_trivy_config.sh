1a6e513a79b5a726ddd6cca4ae3e9a0dc59a95d8
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2023-11-08 10:16:21 +0100

    feat: IaC SAST with trivy config (#104)

---
+++ docker/entrypoints/entrypoint_trivy_config.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+set -e
+
+export TRIVY_NO_PROGRESS=true
+
+if [[ -z "${GITHUB_WORKSPACE}" ]]; then
+  if [[ -z "${CI_PROJECT_DIR}" ]]; then
+    WORKSPACE=.
+  else
+    WORKSPACE="${CI_PROJECT_DIR}"
+  fi
+else
+  WORKSPACE="${GITHUB_WORKSPACE}"
+fi
+
+export SO_FILE_NAME="${REPORT_NAME}"
+export SO_PARSER_NAME="SARIF"
+
+echo ----------------------------------------
+echo Trivy Config
+echo - TARGET:             "$TARGET"
+echo - REPORT_NAME:        "$REPORT_NAME"
+echo - RUN_DIRECTORY:      "$RUN_DIRECTORY"
+if [[ -n "$FURTHER_PARAMETERS" ]]; then
+  echo - FURTHER_PARAMETERS: "$FURTHER_PARAMETERS"
+fi
+
+cd "$RUN_DIRECTORY"
+trivy config $FURTHER_PARAMETERS --quiet --exit-code 0 --format sarif --output "$WORKSPACE/$REPORT_NAME" "$TARGET"
+cd "$WORKSPACE"
+
+if [ "$SO_UPLOAD" == "true" ]; then
+  source file_upload_observations.sh
+fi
+
+exit 0
