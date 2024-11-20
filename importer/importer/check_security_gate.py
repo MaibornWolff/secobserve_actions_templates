d15c2401857055841b487d2f25d4a9eb6c4f4ef2
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2024-11-20 16:01:55 +0000

    fix: exit code 0 for disabled security gate (#225)

--- importer/importer/check_security_gate.py
+++ importer/importer/check_security_gate.py
@@ -11,14 +11,15 @@ def check_security_gate():
         product = api.get_product()
         if product.get("security_gate_passed") == None:
             print(f"Product {product.get('name')}: Security gate DISABLED")            
-            exit(1)
+            exit(0)
 
-        if product.get("security_gate_passed") == False:
-            print(f"Product {product.get('name')}: Security gate FAILED")            
-            exit(1)
+        if product.get("security_gate_passed") == True:
+            print(f"Product {product.get('name')}: Security gate PASSED")
+            exit(0)
 
-        print(f"Product {product.get('name')}: Security gate PASSED")
-        exit(0)
+        print(f"Product {product.get('name')}: Security gate FAILED")            
+        exit(1)
+
     except Exception as e:
         print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
         print(f"{e.__class__.__name__}: {str(e)}")
