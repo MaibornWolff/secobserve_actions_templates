635a1a25d97624bdb61461d115d07967fef73516
Stefan Fleckenstein <stefan@fleckenstein.co.uk>
2025-09-18 19:57:57 +0100

    chore: move virtual environment to Python scripts

--- docker/entrypoints/entrypoint_upload_sbom.sh
+++ docker/entrypoints/entrypoint_upload_sbom.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 set -e
-source /.venv/bin/activate
 source file_upload_sbom.sh
-deactivate
--- importer/bin/check_security_gate.sh
+++ importer/bin/check_security_gate.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+source /.venv/bin/activate
 export PYTHONPATH="$VIRTUAL_ENV/lib/python3.13/site-packages:/usr/local/importer"
 python -m importer.check_security_gate
+deactivate
